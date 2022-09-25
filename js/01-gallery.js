7a44cd9117c6c459d7346d5a9ae020d6ef331053
Ihor Chubov <ihor.chub@gmail.com>
2022-09-25 22:26:30 +0300

    adds a solution to the task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__link" href=${original}>
+    <img
+      class="gallery__image"
+      src=${preview}
+      data-source=${original}
+      alt=${description}
+    />
+  </a>`
+  )
+  .join(' ');
+
+gallery.insertAdjacentHTML('beforeend', markup);
+
+gallery.addEventListener('click', selectImg);
+
+function selectImg(event) {
+  event.preventDefault();
+  const bigImg = event.target.dataset.source;
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(`<img src=${bigImg}>`, {
+    onShow: instance => {
+      window.addEventListener('keydown', instanceCloseByEscape, { once: true });
+      function instanceCloseByEscape(evt) {
+        if (evt.code === 'Escape') {
+          instance.close();
+        }
+      }
+      // window.addEventListener('keydown', instanceCloseByEscape);
+      // function instanceCloseByEscape(evt) {
+      //   if (evt.code === 'Escape') {
+      //     instance.close(() => window.removeEventListener('keydown', instanceCloseByEscape));
+      //     console.log(evt.code);
+      //   }
+      // }
+    },
+  });
+  instance.show();
+}
