3726da527514b8b8eb10863929b46928fe279b84
Ihor Chubov <ihor.chub@gmail.com>
2022-09-26 15:40:32 +0300

    add task #2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href=${original}>
+      <img class="gallery__image" src=${preview} alt=${description} title=${description} />
+      </a>`
+  )
+  .join(' ');
+
+gallery.insertAdjacentHTML('beforeend', markup);
+
+gallery.addEventListener('click', selectImg, { once: true });
+
+function selectImg(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const lightbox = new SimpleLightbox('.gallery .gallery__item', {
+    captionDelay: 250,
+    animationSpeed: 200,
+    fadeSpeed: 150,
+  });
+}
